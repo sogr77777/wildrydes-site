1c81323ef8a741124c3be03e6d3d924ecb7dd43c
sam li <sam.li.co@gmail.com>
2021-03-30 17:47:23 -0500

    new region and etc

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
-        region: 'eu-west-1'
+        userPoolId: 'us-east-1_ipO0QGuXG',
+        userPoolClientId: '22oq2urglj80ntlhjnfnos6734',
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
