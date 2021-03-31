3858bdb482409d5ac38b3da762ea43f097e4d023
sam li <sam.li.co@gmail.com>
2021-03-30 21:13:39 -0500

    add login1

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1'
     },
     api: {
-        invokeUrl: ': https://t7bsu88h3m.execute-api.us-east-1.amazonaws.com/prod'
+        invokeUrl: 'https://t7bsu88h3m.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
